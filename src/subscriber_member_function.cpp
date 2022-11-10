49aeb05565aa3e02fb1c1f3d6e7414688e7282d7
bharadwaj-chukkala <bchukkal@terpmail.umd.edu>
2022-11-10 01:07:41 -0500

    added listener node functionality

--- src/subscriber_member_function.cpp
+++ src/subscriber_member_function.cpp
@@ -1,10 +1,34 @@
-#include <cstdio>
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-int main(int argc, char ** argv)
+#include "../include/beginner_tutorials/subscriber_function.hpp"
+
+MinimalSubscriber::MinimalSubscriber()
+  : Node("minimal_subscriber") {
+     subscription_ = this->create_subscription<std_msgs::msg::String>(
+       "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+}
+
+void MinimalSubscriber::topic_callback(
+  const std_msgs::msg::String::SharedPtr msg) const {
+  RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+}
+
+int main(int argc, char * argv[])
 {
-  (void) argc;
-  (void) argv;
-
-  printf("hello world beginner_tutorials package\n");
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
   return 0;
 }
