309232d568bcadc68e7e7c2aee5aec107b7b5c85
bharadwaj-chukkala <bchukkal@terpmail.umd.edu>
2022-11-10 01:07:00 -0500

    added talker node functionality

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -1,10 +1,37 @@
-#include <cstdio>
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
+#include "../include/beginner_tutorials/publisher_function.hpp"
 
-  printf("hello world beginner_tutorials package\n");
+MinimalPublisher::MinimalPublisher()
+    : Node("minimal_publisher"), count_(0) {
+       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+       timer_ = this->create_wall_timer(500ms,
+        std::bind(&MinimalPublisher::timer_callback, this));
+}
+
+void MinimalPublisher::timer_callback() {
+    auto message = std_msgs::msg::String();
+    message.data = "Hey, This is Bharadwaj, ID:  "
+             + std::to_string(count_++);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+}
+
+int main(int argc, char * argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
   return 0;
 }
