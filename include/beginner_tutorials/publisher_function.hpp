652aca8b466fd5dbb169cdfae25905a15f615576
bharadwaj-chukkala <bchukkal@terpmail.umd.edu>
2022-11-15 18:41:36 -0500

    added add function and service intialization

--- include/beginner_tutorials/publisher_function.hpp
+++ include/beginner_tutorials/publisher_function.hpp
@@ -15,8 +15,11 @@
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "example_interfaces/srv/add_two_ints.hpp"
 
 using namespace std::chrono_literals;
+using std::placeholders::_1;          // for use with binding Class member
+using std::placeholders::_2;          // callback function
 
 /**
  * @brief Class (subclass of Node) and uses std::bind() to register a member function as a callback from the timer.
@@ -28,7 +31,10 @@ class MinimalPublisher : public rclcpp::Node {
 
  private:
   void timer_callback();
+  void add(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request>,
+          std::shared_ptr<example_interfaces::srv::AddTwoInts::Response>);
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service;
   size_t count_;
 };
