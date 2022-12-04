5043a387b24410052da0b71040447d193b5fbb05
bharadwaj-chukkala <bchukkal@terpmail.umd.edu>
2022-12-04 03:26:54 -0500

    modified nodes to broadcast tf frames

--- src/client_node.cpp
+++ src/client_node.cpp
@@ -24,7 +24,7 @@
 #include <cstdlib>
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
-#include "example_interfaces/srv/add_two_ints.hpp"
+#include "../srv/add_two_ints.hpp"
 
 using namespace std::chrono_literals;
 
@@ -37,10 +37,10 @@ int main(int argc, char **argv) {
   }
 
   std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_client");
-  rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client =
-    node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+  rclcpp::Client<::AddTwoInts>::SharedPtr client =
+    node->create_client<srv::AddTwoInts>("add_two_ints");
 
-  auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
+  auto request = std::make_shared<srv::AddTwoInts::Request>();
   request->a = atoll(argv[1]);
   request->b = atoll(argv[2]);
 
